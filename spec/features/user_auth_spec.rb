8cc6137dcccfe666ff98d444e15f5a8667562f02
Luke Bartel <bartelluke@gmail.com>
2014-08-19 15:01:10 -0600

    initial commit, welcome page

---
+++ spec/features/user_auth_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+feature 'user can got to the register page and register' do
+  scenario 'user registers after visiting splash page' do
+
+    visit '/'
+    expect(page).to have_content "Rant.ly Join"
+
+    click_on "Join"
+
+    fill_in "Username", :with => "taquito"
+    fill_in "Password", :with => "tacos"
+    fill_in "First name", :with => "taco"
+    fill_in "Last name", :with => "grande"
+
+    fill_in "Bio", :with => "I'm spicy"
+
+    choose('A Radio Button')
+
+    click_on "Register"
+
+
+
+  end
+end
